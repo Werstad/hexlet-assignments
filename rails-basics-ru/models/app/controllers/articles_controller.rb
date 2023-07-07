9df15c7b01600290a01951a3bb3c4ad40727ecb6
@hexlet/cli <support@hexlet.io>
2023-07-07 22:46:51 +0700

    submit rails-basics-ru/models

--- rails-basics-ru/models/app/controllers/articles_controller.rb
+++ rails-basics-ru/models/app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all.order(created_at: :desc)
+    @articles = Article.all
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @articles = Article.find params[:id]
   end
 end
