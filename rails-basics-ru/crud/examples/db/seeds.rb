21970fdb93302e78b99a5a43c53143e7ed758afa
@hexlet/cli <support@hexlet.io>
2023-07-28 22:54:58 +0700

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/examples/db/seeds.rb
+++ rails-basics-ru/crud/examples/db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |i|
-  article_index = i + 1
+10.times do |_i|
   Article.create(
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraphs.join("\n\n"),
