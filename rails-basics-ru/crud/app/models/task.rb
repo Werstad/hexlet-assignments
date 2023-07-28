d91a5247814e8b71d3bb7d02a3bccb25f2043555
@hexlet/cli <support@hexlet.io>
2023-07-28 22:11:44 +0700

    submit rails-basics-ru/crud

---
+++ rails-basics-ru/crud/app/models/task.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  completed   :boolean
+#  creator     :string
+#  description :text
+#  name        :string
+#  performer   :string
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Task < ApplicationRecord
+  validates :name, :status, :creator, presence: true
+  validates :completed, inclusion: { in: [true, false] }
+end
---
+++ rails-basics-ru/crud/db/seeds.rb
@@ -0,0 +1,10 @@
+10.times do |_index|
+  Task.create(
+    name: Faker::Name.unique.name,
+    description: Faker::Movies::HarryPotter.quote,
+    status: Faker::Movies::HarryPotter.spell,
+    creator: Faker::Movies::HarryPotter.character,
+    performer: Faker::Movies::HarryPotter.character,
+    completed: Faker::Boolean.boolean
+  )
+end
