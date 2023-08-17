de34c2d1ee69fcbfe6e7c78496c22373bfc296f6
@hexlet/cli <support@hexlet.io>
2023-08-17 22:32:38 +0700

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/config/routes.rb
+++ rails-basics-ru/crud/config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'home#index'
   # BEGIN
   resources :tasks
-
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- rails-basics-ru/crud/db/schema.rb
+++ rails-basics-ru/crud/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_729_164_941) do
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.string 'status'
-    t.string 'creator'
-    t.string 'performer'
-    t.boolean 'completed'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_091949) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "status"
+    t.string "creator"
+    t.string "performer"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
