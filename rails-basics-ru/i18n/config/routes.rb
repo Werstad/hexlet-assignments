5672f493fcaa9f9443f8520b8c0068bdc546d8a4
@hexlet/cli <support@hexlet.io>
2023-09-23 23:21:12 +0700

    submit rails-basics-ru/i18n

---
+++ rails-basics-ru/i18n/config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  scope '(:locale)', locale: /en|ru/ do
+    root 'home#index'
+
+    resources :posts do
+      scope module: :posts do
+        resources :comments, only: %i[edit create update destroy]
+      end
+    end
+  end
+end
